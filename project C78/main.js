05633d07f6dad89b935de39e448d086c566f0ab3
vanshkyadav <78957763+vanshkyadav@users.noreply.github.com>
2021-05-14 12:16:31 +0530

    Add files via upload

---
+++ project C78/main.js
@@ -0,0 +1,6 @@
+var photos=["1620635055646.jpg","1620635911703.jpg","1620638700273.jpg","1620644301415.jpg"];
+var names=["Mom","Dad","Me","Sister"];
+var i=0;
+function change (){i++;
+if(i>4){i=0};
+}
